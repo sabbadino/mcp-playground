3a967a0645714d56bee5f205612b02e039b121bb
Enrico Sabbadin <esabbadin@codearchitects.com>
2025-05-06 14:39:37 +0200

    WIP

---
+++ weather-mcp-server-dapr/weather-mcp-server-dapr/Program.cs
@@ -0,0 +1,47 @@
+using Google.Api;
+using Microsoft.Extensions.DependencyInjection;
+using weather_mcp_server_dapr;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddDapr();
+builder.Services
+    .AddMcpServer().WithHttpTransport(o =>
+    {
+        
+    })
+    .WithToolsFromAssembly();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHttpClient();
+//builder.Services.AddOpenTelemetry()
+//    .WithTracing(b => b.AddSource("*")
+//        .AddAspNetCoreInstrumentation()
+//        .AddHttpClientInstrumentation().AddConsoleExporter())
+//    .WithMetrics(b => b.AddMeter("*")
+//        .AddAspNetCoreInstrumentation()
+//        .AddHttpClientInstrumentation().AddConsoleExporter())
+//    .WithLogging(b => b.AddConsoleExporter());
+    
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+#if !DEBUG
+app.UseHttpsRedirection();
+#endif
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapMcp();
+
+app.Run();
